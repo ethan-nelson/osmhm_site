21994678d716f75cbc4dbf29b49edc34885244b2
Ethan Nelson <ethan-nelson@users.noreply.github.com>
2016-09-22 23:22:46 +0200

    Update sample processor script

--- runprocessor.py
+++ runprocessor.py
@@ -1,5 +1,32 @@
 import osmhm
+import osmhm.config
 
-osmhm.run(history=False)
+def send_grid_example(to, subject, message):
+    import sendgrid
+    import os
 
-print 'Processor stopped.'
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    data = {
+      "personalizations": [
+        {
+          "to": [
+            {
+              "email": to
+            }
+          ],
+          "subject": subject
+        }
+      ],
+      "from": {
+        "email": osmhm.config.email_user
+      },
+      "content": [
+        {
+          "type": "text/plain",
+          "value": message
+        }
+      ]
+    }
+    response = sg.client.mail.send.post(request_body=data)
+
+osmhm.run(notification=True, notifier=send_grid_example)
